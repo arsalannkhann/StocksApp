b4992f4a629d7d28cdd738130853629c1218545b
arsalannkhann <167637419+arsalannkhann@users.noreply.github.com>
2025-08-03 23:47:44 +0530

    Remove all files from repository

--- scripts/start_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting Backend Server..."
-
-cd /Users/arsalankhan/Documents/RESEARCH/StocksApp/backend
-source venv/bin/activate
-export PYTHONPATH=$(pwd)
-python app.py
--- scripts/start_frontend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting Frontend..."
-cd /Users/arsalankhan/Documents/RESEARCH/StocksApp/frontend
-
-npm run dev
